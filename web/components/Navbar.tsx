82f9b8d3f3561a391f03f059cf56d18f61cf2318
wade-loccisano <wade.loccisano@gmail.com>
2025-01-11 15:24:20 -0500

    Add components and layout

---
+++ web/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <header className="section_container !py-0">
+            <nav className="text-center">
+                <div className="p-2">
+                    Navbar-arino
+                </div>
+                <button className='p-2 border'>
+                    Toggle Theme
+                </button>
+            </nav>
+        </header >
+    )
+}
+
+export default Navbar
