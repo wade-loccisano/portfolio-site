715aef12b60a33d3839513eb13c433ab9d0b3968
wade-loccisano <wade.loccisano@gmail.com>
2025-01-27 15:42:38 -0500

    Add ProjectTitleSection component

---
+++ web/components/ProjectTitleSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectTitleSection = () => {
+    return (
+        <section className="section_container flex justify-center">
+            <div className="text-xl md:text-2xl border-b-2 border-slate-400 font-bold text-center w-[90%] lg:w-[50%] xl:w-[30%]">
+                <div>
+                    Projects
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ProjectTitleSection
