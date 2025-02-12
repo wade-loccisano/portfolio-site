bd0cb461fd78e674cae956f553cf558eff9c97ae
wade-loccisano <wade.loccisano@gmail.com>
2025-02-12 13:09:24 -0500

    Changes to page layout.

--- web/components/ProjectTitleSection.tsx
+++ web/components/ProjectTitleSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProjectTitleSection = () => {
     return (
-        <section className="section_container flex justify-center">
+        <section className="section_container flex justify-center pb-8">
             <div className="text-xl md:text-2xl border-b-2 border-slate-400 font-bold text-center w-[90%] lg:w-[50%] xl:w-[30%]">
                 <div>
                     Projects
